fac010c19ba989ec7e404a9b7aba4736a12af9f6
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-03-08 14:03:22 +0200

    printing repos statuses

--- src/lib/format.js
+++ src/lib/format.js
@@ -17,8 +17,17 @@ const warning = (text, label = 'WARN') => {
   console.log(`${text}${chalk.white.bgRed.bold(` ${label} `)}`);
 };
 
+const printStatuses = (statuses) => {
+  statuses.forEach(status => {
+    console.log(`\n${chalk.yellow(status.context)}`);
+    console.log(status.description);
+    console.log(status.target_url);
+  });
+};
+
 module.exports = {
   stringBuilder,
+  printStatuses,
   success,
   failure,
   warning
--- src/plugins/tests.js
+++ src/plugins/tests.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-const { stringBuilder, success, failure } = require('../lib/format');
+const R = require('ramda');
+const { stringBuilder, printStatuses, success, failure } = require('../lib/format');
 const { error } = require('../lib/result');
 
 module.exports = async (module) => {
@@ -22,11 +23,30 @@ module.exports = async (module) => {
   const testDirectory = repoTree.tree.find(({ path, type }) => path.includes('test') && type === 'tree');
   const testOutput = stringBuilder('\nChecking if module has appropriate testing').withPadding(66);
 
+  const commitHash = commitHistory[0].sha;
+  const { data: repoStatuses } = await axios.get(`https://api.github.com/repos/${githubTarget}/commits/${commitHash}/statuses`);
+
+  const parseStatuses = R.pipe(
+    R.filter((status) => status.state !== 'pending'),
+    R.groupBy((status) => status.context),
+    R.mapObjIndexed(
+      R.pipe(
+        R.sort((a, b) => new Date(b) - new Date(a)),
+        R.head
+      )
+    )
+  );
+
+  const statusRecords = parseStatuses(repoStatuses);
+  const statuses = Object.keys(statusRecords).map(key => statusRecords[key]);
+
   if (npmTestCommand || testDirectory) {
     success(testOutput.get());
+    printStatuses(statuses);
     return null;
   }
 
   failure(testOutput.get());
+  printStatuses(statuses);
   return error(`The "${module.name}" seems that is lacking appropriate testing (https://www.github.com/${githubTarget})`);
 };
