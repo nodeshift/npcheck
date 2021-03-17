c52bee36b8f298fed543bb8560dea17094748cc6
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-03-17 16:01:26 +0200

    adding cache when fetching from github

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 
+const cache = new Map();
+
 const fetchGithub = async (target, token, override = false) => {
+  // check if we already have the response in cache
+  if (cache.has(target)) return cache.get(target);
+
   const github = 'https://api.github.com';
   const url = override ? target : `${github}${target}`;
   const options = {
@@ -9,6 +14,8 @@ const fetchGithub = async (target, token, override = false) => {
     }
   };
   const response = await axios.get(url, options);
+  // save response in cache for future usage
+  cache.set(target, response);
   return response.data;
 };
 
