3a61c42fef64ee072f0193b74ad6a0a2d32475a5
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-05-19 12:24:24 +0300

    Adding license rules to top-level modules (#25)
    * Adding license rules to top-level modules
    
    * upgrading to v0.1.11

--- src/plugins/license.js
+++ src/plugins/license.js
@@ -1,21 +1,41 @@
-const { stringBuilder, success, failure } = require('../lib/format');
-const { error } = require('../lib/result');
+const { stringBuilder, success, failure, warning } = require('../lib/format');
+const { error, passThroughError } = require('../lib/result');
+const { matchLicenses } = require('../lib/regex');
 
 const licensePlugin = async (module, config) => {
+  // Licenses that we'll consider passing
   const licenses = config.licenses?.allow || [];
-  const isLicenseAllowed = licenses.find((name) => name === module?.license);
+  const licensesLocal = config.licenses?.rules[module.name]?.allow || [];
+
+  // Licenses that will pass but with a warning
+  const licensesForcePass = config.licenses?.rules[module.name]?.override || [];
+
+  const isPassing = [...licenses, ...licensesLocal].find((name) =>
+    matchLicenses(module.license, name)
+  );
 
   const output = stringBuilder('\nChecking top-level license').withPadding(66);
 
-  if (!isLicenseAllowed) {
-    failure(output.get());
-    return error(
-      `The module "${module.name}" is under the non-acceptable license "${module.license}".`
+  if (isPassing) {
+    success(output.get());
+    return null;
+  }
+
+  const isForcePassing = licensesForcePass.find((name) =>
+    matchLicenses(module.license, name)
+  );
+
+  if (isForcePassing) {
+    warning(output.get());
+    return passThroughError(
+      `The module "${module.name}" is under the the yet undetermined license "${module.license}". (Manual review needed)`
     );
   }
 
-  success(output.get());
-  return null;
+  failure(output.get());
+  return error(
+    `The module "${module.name}" is under the non-acceptable license "${module.license}".`
+  );
 };
 
 module.exports = licensePlugin;
