0d343057ecd499f9bf3a83d026ee156982f38d07
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-06-29 15:59:57 +0300

    adding tests for the license plugin

---
+++ test/plugins/license.test.js
@@ -0,0 +1,107 @@
+/* eslint-env jest */
+
+const { success, warning, failure } = require('../../src/lib/format');
+const licensePlugin = require('../../src/plugins/license');
+
+jest.mock('../../src/lib/format', () => ({
+  ...jest.requireActual('../../src/lib/format'),
+  failure: jest.fn(),
+  success: jest.fn(),
+  warning: jest.fn()
+}));
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+it('should return null when license is globally accepted', async () => {
+  // create a sample env with some dummy data
+  const testEnv = {
+    pkg: {
+      name: 'test',
+      license: 'MIT'
+    },
+    config: {
+      licenses: {
+        allow: ['MIT', 'Apache-2.0'],
+        rules: {}
+      }
+    }
+  };
+
+  const result = await licensePlugin(testEnv.pkg, testEnv.config);
+
+  expect(result).toBe(null);
+  expect(success).toHaveBeenCalled();
+});
+
+it('should return null when the license is locally accepted', async () => {
+  // create a sample env with some dummy data
+  const testEnv = {
+    pkg: {
+      name: 'test',
+      license: 'Apache-2.0'
+    },
+    config: {
+      licenses: {
+        allow: ['MIT'],
+        rules: {
+          test: {
+            allow: ['Apache-2.0']
+          }
+        }
+      }
+    }
+  };
+
+  const result = await licensePlugin(testEnv.pkg, testEnv.config);
+
+  expect(result).toBe(null);
+  expect(success).toHaveBeenCalled();
+});
+
+it('should return a warning when the license is locally overridden', async () => {
+  // create a sample env with some dummy data
+  const testEnv = {
+    pkg: {
+      name: 'test',
+      license: 'Apache-2.0'
+    },
+    config: {
+      licenses: {
+        allow: ['MIT'],
+        rules: {
+          test: {
+            override: ['Apache-2.0']
+          }
+        }
+      }
+    }
+  };
+
+  const result = await licensePlugin(testEnv.pkg, testEnv.config);
+
+  expect(result.type).toBe('warning');
+  expect(warning).toHaveBeenCalled();
+});
+
+it('should return an error when the license is not accepted', async () => {
+  // create a sample env with some dummy data
+  const testEnv = {
+    pkg: {
+      name: 'test',
+      license: 'Apache-2.0'
+    },
+    config: {
+      licenses: {
+        allow: ['MIT'],
+        rules: {}
+      }
+    }
+  };
+
+  const result = await licensePlugin(testEnv.pkg, testEnv.config);
+
+  expect(result.type).toBe('error');
+  expect(failure).toHaveBeenCalled();
+});
