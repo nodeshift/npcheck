5fb15e7cd40ee12f0686666ab6d142f9d52e63db
Alex Alikiotis <alexalikiotis5@gmail.com>
2021-07-08 13:26:48 +0300

    fix: license field issue (#38)

--- test/plugins/license.test.js
+++ test/plugins/license.test.js
@@ -35,6 +35,32 @@ it('should return null when license is globally accepted', async () => {
   expect(success).toHaveBeenCalled();
 });
 
+it('should handle the case where the license field is an array', async () => {
+  // create a sample env with some dummy data
+  const testEnv = {
+    pkg: {
+      name: 'test',
+      licenses: [
+        {
+          type: 'MIT',
+          url: 'https://raw.github.com/test/test/master/licence'
+        }
+      ]
+    },
+    config: {
+      licenses: {
+        allow: ['MIT', 'Apache-2.0'],
+        rules: {}
+      }
+    }
+  };
+
+  const result = await licensePlugin(testEnv.pkg, testEnv.config);
+
+  expect(result).toBe(null);
+  expect(success).toHaveBeenCalled();
+});
+
 it('should return null when the license is locally accepted', async () => {
   // create a sample env with some dummy data
   const testEnv = {
