6ba8b48486d8545649e864ddc1bbe47e36d6b6fe
Michael Dawson <mdawson@devrus.com>
2022-10-17 23:19:29 -0400

    feat: special case BSD* to allow match with BSD
    Special case BSD* to allow match with BSD
    as regex match does not allow special regex chars
    at end of licence.
    
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- src/lib/regex.js
+++ src/lib/regex.js
@@ -4,6 +4,9 @@ const escapeRegExp = (string) => {
 
 // 'MIT OR Apache.2.0'.match('MIT')
 const matchLicenses = (license, target) => {
+  // allow BSD* to match BSD
+  if (license === 'BSD*') license = 'BSD';
+
   const filter = new RegExp(`\\b${escapeRegExp(target)}\\b`);
   const match = Array.isArray(license)
     ? license.join(' OR ').match(filter)
